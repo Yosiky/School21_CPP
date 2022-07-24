006c0c5356453088a35672ac041c8f52155b92c7
Eladia Estelle <eestelle@at-o5.msk.21-school.ru>
2022-07-24 14:05:31 +0300

    fix bug

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -4,15 +4,17 @@
 void    copy(std::ifstream &in, std::ofstream &out, std::string &s1, std::string &s2)
 {
     size_t      pos;
-    size_t      left = 0;
+    size_t      left;
     const char  *str;
     std::string value;
 
     while (!in.eof())
     {
+        left = 0;
+        pos = std::string::npos;
         std::getline(in, value);
         str = value.c_str();
-        pos = value.find(s1, 0);
+        pos = value.find(s1, left);
         while (pos != std::string::npos)
         {
             out << value.substr(left, pos - left);
@@ -20,37 +22,42 @@ void    copy(std::ifstream &in, std::ofstream &out, std::string &s1, std::string
             left += pos + s1.size();
             pos = value.find(s1, left);
         }
-        out << value.substr(left, value.size()) << std::endl;
+        out << value.substr(left, value.size());
+        if (!in.eof())
+            out << std::endl;
     }
+}
+
+int error(const std::string &str, int num)
+{
+    std::cerr << str << std::endl;
+    return num;
 }
 
 int main(int argc, char **argv)
 {
     if (argc != 4)
-    {
-        std::cerr << "Error: your input haven't 3 arguments" << std::endl;
-        return 1;
-    }
+        return error("Error: your input haven't 3 arguments", 1);
     std::string     fileName(argv[1]);
     std::string     s1(argv[2]);
     std::string     s2(argv[3]);
-    std::ifstream   input(fileName);
-    std::ofstream   output(fileName + ".replace");
-    int             flag = (input.is_open() << 1) + (output.is_open());
+    if (fileName.size() == 0)
+        return error("Error: your name file is empyt", 2); 
     if (s1 == "")
-    {
-        std::cerr << "Error: size string s1 is 0" << std::endl;
-    }
+        return error("Error: size string s1 is 0", 3); 
     else
     {
+        std::ifstream   input(fileName);
+        std::ofstream   output(fileName + ".replace");
+        int             flag = (input.is_open() << 1) + (output.is_open());
         if (flag == 3)
             copy(input, output, s1, s2);
-        else
-            std::cerr << "Error: file not open" << std::endl;
+        if (flag & 2)
+            input.close();
+        if (flag & 1)
+            output.close();
+        if (flag != 3)
+            return error("Error: file not open", 4);
     }
-    if (flag & 2)
-        input.close();
-    if (flag & 1)
-        output.close();
     return 0;
 }
