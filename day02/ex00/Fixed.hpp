cac1fb5069a9fcd18c780e25c1949bc39c393d19
Eladia Estelle <eestelle@at-o5.msk.21-school.ru>
2022-07-24 15:26:14 +0300

    finish ex00

---
+++ day02/ex00/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class   Fixed
+{
+private:
+    int value;
+    static int  widthFloatParth;
+
+public:
+    Fixed(void);
+    Fixed(const Fixed &obj);
+    ~Fixed(void);
+
+    Fixed   &operator=(const Fixed &obj);
+    int     getRawBits(void) const;
+    void    setRawBits(int const raw);
+};
+
+#endif //FIXED_HPP
