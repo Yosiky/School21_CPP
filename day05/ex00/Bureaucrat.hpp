17c399e0840da5a13283a3eab8218e45964d1308
Eladia Estelle <eestelle@at-o7.msk.21-school.ru>
2022-08-04 21:00:49 +0300

    finish ex00

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class   Bureaucrat
+{
+    const std::string   name;
+    int                 grade;
+
+    Bureaucrat(void);
+    Bureaucrat  &operator=(const Bureaucrat &obj);
+    
+    class   GradeTooHighException : public std::exception
+    {
+        virtual const char* what() const throw();
+    };
+
+    class   GradeTooLowException : public std::exception
+    {
+        virtual const char* what() const throw();
+    };
+
+public:
+    Bureaucrat(const std::string &str, int grd);
+    Bureaucrat(const Bureaucrat &obj);
+    ~Bureaucrat(void);
+
+    const std::string   &getName(void) const;
+    int getGrade(void) const;
+
+    void    upGrade(void);
+    void    downGrade(void);
+};
+
+std::ostream    &operator<<(std::ostream &out, const Bureaucrat &obj);
+
+#endif
