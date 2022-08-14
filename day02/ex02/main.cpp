580d8d7d34df2f7d25c00e6281f52980c127b49f
eestelle <yosiky@list.ru>
2022-08-14 21:17:07 +0300

    day02 ex02 changed main

--- day02/ex02/main.cpp
+++ day02/ex02/main.cpp
@@ -3,11 +3,12 @@
 
 int main( void ) {
     Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    std::cout << b / Fixed(2) << std::endl;
-    std::cout << b - Fixed(2) << std::endl;
-    std::cout << b + Fixed(2) << std::endl;
-    std::cout << (b + Fixed(2)) * Fixed(2) << std::endl;
+    Fixed c(2);
+    Fixed const b( Fixed( 5.05f ) * c );
+    std::cout << b / c << std::endl;
+    std::cout << b - c << std::endl;
+    std::cout << b + c << std::endl;
+    std::cout << (b + c) * c << std::endl;
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << a << std::endl;
@@ -15,5 +16,6 @@ int main( void ) {
     std::cout << a << std::endl;
     std::cout << b << std::endl;
     std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << Fixed::min( a, b ) << std::endl;
     return 0;
 }
