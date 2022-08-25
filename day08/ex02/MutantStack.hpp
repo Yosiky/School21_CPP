e40008d486bb5650da47d5962c0a252329987087
Eladia Estelle <eestelle@at-m8.msk.21-school.ru>
2022-08-25 18:57:46 +0300

    day08: ex02 finished

--- day08/ex02/MutantStack.hpp
+++ day08/ex02/MutantStack.hpp
@@ -10,19 +10,55 @@ class   MutantStack : public std::stack<T, Container>
 {
 public:
     using   std::stack<T, Container>::c;
-    typedef typename std::stack<T, Container>::container_type::iterator reverse_iterator;
-    typedef typename std::stack<T, Container>::container_type::reverse_iterator iterator;
+    typedef typename std::stack<T, Container>::container_type::iterator iterator;
+    typedef typename std::stack<T, Container>::container_type::const_iterator citerator;
+    typedef typename std::stack<T, Container>::container_type::reverse_iterator riterator;
+    typedef typename std::stack<T, Container>::container_type::const_reverse_iterator criterator;
     //MutantStack<T> &operator=(const MutantStack<T> &obj);
 
     iterator   begin(void)
     {
-        return c.rbegin();
+        return c.begin();
     }
 
     iterator  end(void)
     {
+        return c.end();
+    }
+
+    citerator   cbegin(void)
+    {
+        return c.cbegin();
+    }
+                           
+    citerator  cend(void)
+    {
+        return c.cend();
+    }
+
+    riterator   rbegin(void)
+    {
+        return c.rbegin();
+    }
+                             
+    riterator  rend(void)
+    {
         return c.rend();
     }
+                             
+    criterator   crbegin(void)
+    {
+        return c.crbegin();
+    }
+                           
+    criterator  crend(void)
+    {
+        return c.crend();
+    }
+
+
+
+
 };
 
 //# include "MutantStack.tpp"
